317f8d1e5e915cf6fb0703cb38482ff34055a05d
CruelBlade <redeptikon@mail.ru>
2020-04-27 02:29:01 +0300

    first button

--- MihailGor/src/index.jsx
+++ MihailGor/src/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HelloMessage from "./components/HelloMessage";
+import Button from "./components/Button";
+
+<App>
+  <HelloMessage name="Саша" />
+  <p/>
+  <Button />
+</App>,
 
 ReactDOM.render(
   <HelloMessage name="Саша" />,
