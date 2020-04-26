83422063c44cc6a94504b6b221b2a854018c511f
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2020-04-26 13:14:42 +0300

    done

--- petukhov/src/components/HelloMessage.jsx
+++ petukhov/src/components/HelloMessage.jsx
@@ -12,16 +12,23 @@ class HelloMessage extends React.Component {
   }
 
   addMessage() {
-    this.setState((prev) => ({ messages: [...prev.messages, "add"] }));
+    this.setState(prev => ({ messages: [...prev.messages, "Нормально"] }));
   }
 
   render() {
     const { messages } = this.state;
+    const messagesList = messages.map((item, index) => {
+        return (
+            <li key = {index}>{item}</li>
+            )
+    });
     return (
       <div>
         <h2>Hello, {this.props.name}!</h2>
-        <Example />
-        <button onClick={this.addMessage}>Click</button>
+        <button onClick={this.addMessage}>Add list item</button>
+        <ul>
+            {messagesList}
+        </ul>
       </div>
     );
   }
