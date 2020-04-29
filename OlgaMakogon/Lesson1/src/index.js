bce0b40f32e66109a6a7e08dc38647058f7b45de
OVMako <58699743+OVMako@users.noreply.github.com>
2020-04-29 16:25:50 +0300

    Revert "кнопка"
    This reverts commit dc817eac7002b78afcf8923e1891dd630ee5ee0e.

--- OlgaMakogon/Lesson1/src/index.js
+++ OlgaMakogon/Lesson1/src/index.js
@@ -3,17 +3,3 @@ import ReactDOM from 'react-dom';
 import HelloMessage from "./components/HelloMassage"
   
   ReactDOM.render(React.createElement(HelloMessage, { name: "Саша" }), document.getElementById('hello-example'));
-
-  const Massenger = ({messages}) => {
-      const sendMassage = () => {
-        messages.push({name:"test", content:"Нормально"});
-        renderApp;
-      }
-      return (
-          <div>
-          <button onClick={sendMassage}>Send message</button>
-          </div>
-      )
-  }
-  function renderApp() { (Massenger, { name:"test" }), document.getElementById('hello-example'));
-  } 
