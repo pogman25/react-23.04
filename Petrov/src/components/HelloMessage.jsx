cc9dba123ce7d01e043e888085daff74adb7a058
WetShark <7393422@mail.ru>
2020-04-29 14:35:55 +0300

    dz-2

--- Petrov/src/components/HelloMessage.jsx
+++ Petrov/src/components/HelloMessage.jsx
@@ -1,31 +1,42 @@
 import React from "react";
 import Example from "./Example";
-import ListMessages from "./ListMessages";
+import Message from "./Message";
+import FormMessage from "./FormMessage";
 
 
 class HelloMessage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
+state = {
       messages: [],
     };
-// Привязка необходима, чтобы сделать this доступным в коллбэке
-    this.addMessage = this.addMessage.bind(this);
+
+
+  addMessage = (name, text) => {
+    this.setState((prev) => ({ messages: [...prev.messages, {id: (prev.messages.length+1), message: text, author: name}] }));
   }
 
-  addMessage() {
-    this.setState((prev) => ({ messages: [...prev.messages, {id: (prev.messages.length+1), message: "Нормально"}] }));
+  componentDidUpdate() {
+    const { messages } = this.state;
+    if (messages[messages.length - 1].author !== "Бот") {
+      setTimeout(() => {
+        this.setState(this.addMessage('Бот', 'привет, я Бот'));
+      }, 1000);
+    }
   }
+  
+  addNewMessage = (e) => {
+   this.addMessage(e.author, e.text);
+  };
 
   render() {
-   // const { messages } = this.state;
+    const { messages } = this.state;
+    const { name } = this.props;
     return (
       <div>
-        <h2>Привет, {this.props.name}</h2>
+        <h2>Привет, {name}</h2>
         <Example />
-        <button onClick={this.addMessage}> Click</button>
-        <ListMessages words={this.state.messages} />
+        <FormMessage addNewMessage={this.addNewMessage} />
+        <button onClick={ () => this.addMessage('me', 'Нормально')}> Click</button>
+        <Message words={messages} />
       </div>
     );
   }
