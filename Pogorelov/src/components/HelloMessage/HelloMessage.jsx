2f43075a32b19f861ca6737d027d1a24e16c5280
Погорелов Александр <a.pogorelov@zebrains.team>
2020-04-30 22:41:39 +0300

    add theme

--- Pogorelov/src/components/HelloMessage/HelloMessage.jsx
+++ Pogorelov/src/components/HelloMessage/HelloMessage.jsx
@@ -1,28 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles, createStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import FormMessage from '../FormMessage';
 import Messages from '../Messages/Messages';
-import styles from './index.css';
 
-const muiStyles = createStyles({
-  paper: {
-    marginTop: 60,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-});
+const muiStyles = theme => {
+  console.log(theme);
+  return {
+    paper: {
+      marginTop: theme.spacing(7),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  };
+};
 
 class HelloMessage extends Component {
   state = {
     messages: [],
-  };
-
-  addMessage = () => {
-    this.setState(({ messages }) => ({
-      messages: [...messages, { text: 'привет', author: 'Pog' }],
-    }));
   };
 
   componentDidUpdate() {
@@ -37,6 +33,12 @@ class HelloMessage extends Component {
     }
   }
 
+  addMessage = () => {
+    this.setState(({ messages }) => ({
+      messages: [...messages, { text: 'привет', author: 'Pog' }],
+    }));
+  };
+
   addNewMessage = data => {
     this.setState(({ messages }) => ({ messages: [...messages, data] }));
   };
@@ -50,8 +52,12 @@ class HelloMessage extends Component {
         <h2 className="title">{`Привет, ${name} ${lastname}`}</h2>
         <Messages messages={messages} />
         <FormMessage addNewMessage={this.addNewMessage} />
-        <button onClick={this.addMessage}>Click</button>
-        <button onClick={this.toggle}>Visible</button>
+        <button type="button" onClick={this.addMessage}>
+          Click
+        </button>
+        <button type="button" onClick={this.toggle}>
+          Visible
+        </button>
       </div>
     );
   }
--- Pogorelov/src/index.jsx
+++ Pogorelov/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from '@material-ui/core/styles';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import HelloMessage from './components/HelloMessage';
 import './index.css';
 
-const theme = {
+const theme = createMuiTheme({
   background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-};
+});
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
