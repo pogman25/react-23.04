f9508a1e3cdea3075eb2bd6fe59931f98f9fc8a3
Aleksey Skhomenko <a@skhom.ru>
2020-05-01 22:10:58 +0700

    useInterval custom hook !
    now message component have smart logic for rerender. No more crutch solutions.

--- SkhomEnko/Project/src/components/Message.jsx
+++ SkhomEnko/Project/src/components/Message.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from "prop-types"
 import dayjs from 'dayjs'
 import './Message.css'
 import clsx from 'clsx'
+import useInterval from './hooks/useInterval'
 
 function Message(props) {
     const {
@@ -13,10 +14,14 @@ function Message(props) {
       timestamp,
       isMine
     } = props.data
-
-    const fromTimestamp = dayjs(timestamp).fromNow()
+    
+    const [fromTimestamp, setFromTimestamp] = useState(dayjs(timestamp).fromNow())
     const formatedTimestamp = dayjs(timestamp).format('llll')
 
+    useInterval(() => {
+      if (fromTimestamp !== dayjs(timestamp).fromNow()) setFromTimestamp(dayjs(timestamp).fromNow())
+    }, 2000)
+   
     return (   
       <div className={clsx('message', {'mine': isMine})}>
         {
--- SkhomEnko/Project/src/components/MessageField.jsx
+++ SkhomEnko/Project/src/components/MessageField.jsx
@@ -5,37 +5,20 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 import ru from 'dayjs/locale/ru'
 import localizedFormat from 'dayjs/plugin/localizedFormat'
-// material design
-import Icon from '@material-ui/core/Icon/'
-import AdbIcon from '@material-ui/icons/DeleteOutlined'
 // my imports
 import Message from "./Message"
 // style
 import './MessageField.css'
-
 
 dayjs.extend(relativeTime)
 dayjs.extend(localizedFormat)
 dayjs.locale((window.navigator.userLanguage === 'ru-RU' || window.navigator.language === "ru-RU") ? ru : "en") // меняем локаль moment на русскую).
 
 class MessageField extends React.PureComponent {
-    componentDidMount() {
-        // делаем перерендер раз в 5 сек, это нужно, что бы перерисовать функциональные сообщения,
-        // состояние в них не меняется, но нужно, что-бы менялся рендер (5 сек назад, 1 мин назад и т.д)
-        // как завязать рендер у функциональных компонентов на компутед значение не сообразил.
-        // По сути это основной костыль который я временно оставляю, 
-        // пока не найду лучшего решения, не переписывая message на класс.
-        this.interval = setInterval(() => this.setState({ time: Date.now() }), 5000)
-    }
-
     scrollToBottom = () => {
         this.messagesEnd.scrollIntoView({ behavior: "smooth" }) // скроллинг вниз
     }
-    
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-    // <Icon component={AdbIcon}></Icon>
+
     render () {
         const { messages, uname } = this.props
         return (
