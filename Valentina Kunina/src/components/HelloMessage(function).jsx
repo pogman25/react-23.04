7695619b79c10b08529e194899e25f70e247aaca
Konfeta <Vale@iMac.local>
2020-05-06 18:13:59 +0300

    Домашнее задание к Уроку 2 (дополненное)

--- Valentina Kunina/src/components/HelloMessage(function).jsx
+++ Valentina Kunina/src/components/HelloMessage(function).jsx
@@ -1,30 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, memo, useEffect } from "react";
+import PropTypes from "prop-types";
 import Example from "./Example";
+import Counter from "./Counter";
+import FormMessage from "./FormMessage";
 
-const HelloMessage = () => {
+const initialState = {
+    messages: [
+        { text: "Привет!", author: "User" },
+        { text: "Как дела?", author: "User" },
+    ],
+    name: "Samon",
+    visible: true,
+};
+
+const HelloMessage = ({ lastname }) => {
+    // ({ lastname }) - деструктуризация входящего пропса, можно прописать props,
+    // но тогда обращаться как к объекту - props.lastname
     //state = initialState; вместо классового state используется:
-    [messages, setMessages] = useState(["Привет!", "Как дела?"]);
+
+    const [messages, setMessages] = useState(initialState.messages);
+    const [name, setName] = useState(initialState.name);
+    const [isVisible, setVisibility] = useState(true);
+
+    const toggle = () => {
+        setVisibility((prev) => !prev);
+    };
 
     const addMessage = () => {
-        const { messages } = this.state;
-        this.setState({ messages: [...messages, "Нормально"], name: "Peter" });
+        setMessages([...messages, { text: "Нормально" }]);
     };
+
+    const addNewMessage = (message) => {
+        setMessages([...messages, { text: message.text, author: message.author }]);
+    };
+
+    const changeName = () => {
+        setName("Peter");
+    };
+
+    const reset = () => {
+        setMessages(initialState.messages);
+        setName(initialState.name);
+    };
+
+    const handleClick = () => {
+        addMessage();
+        changeName();
+    };
+
+    useEffect(() => {
+        if (messages[messages.length - 1].author !== "Bot") {
+            setTimeout(() => {
+                setMessages([...messages, { text: "Привет, я Бот!", author: "Bot" }]),
+                    setName("Bot");
+            }, 500);
+        }
+    });
 
     return (
         <div>
-            <h2>Hello {name}</h2>
+            <h2>{`Hello, ${name} ${lastname}!`}</h2>
             <Example />
-            {messages.map((message, idx) => (
-                <div key={idx}>{message}</div>
+            {messages.map(({ text, author }, idx) => (
+                <div key={idx}>{`${author}: ${text}`}</div>
             ))}
-            <button onClick={addMessage}>Ckick</button>
+            <FormMessage addNewMessage={addNewMessage} />
+            <button onClick={handleClick}>Ckick</button>
             <button onClick={reset}>Reset</button>
+            <button onClick={toggle}>Visible</button>
+            {isVisible && <Counter />}
         </div>
     );
-
-    reset = () => {
-        this.setState(initialState);
-    };
 };
 
-export default HelloMessage;
+HelloMessage.defaultProps = {
+    lastname: "Ivanov",
+};
+
+HelloMessage.PropTypes = {
+    name: PropTypes.string.isRequired,
+    lastname: PropTypes.string.isRequired,
+};
+
+export default memo(HelloMessage);
--- Valentina Kunina/src/components/HelloMessage.jsx
+++ Valentina Kunina/src/components/HelloMessage.jsx
@@ -1,37 +1,83 @@
-import React from "react";
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 import Example from "./Example";
+import Counter from "./Counter";
+import FormMessage from "./FormMessage";
 
 const initialState = {
-    messages: ["Привет!", "Как дела?"],
-    name: 'Samon'
-}
+    messages: [
+        { text: "Привет!", author: "User" },
+        { text: "Как дела?", author: "User" },
+    ],
+    name: "Samon",
+    visible: true,
+};
 
-class HelloMessage extends React.Component {
+class HelloMessage extends PureComponent {
     state = initialState;
 
     addMessage = () => {
         const { messages } = this.state;
-        this.setState({ messages: [...messages, "Нормально"], name: 'Peter' });
+        this.setState({
+            messages: [...messages, { text: "Нормально", author: "User" }],
+            name: "Peter",
+        });
+    };
+
+    componentDidUpdate() {
+        const { messages } = this.state;
+        if (messages[messages.length - 1].author !== "Bot") {
+            setTimeout(() => {
+                this.setState({
+                    messages: [...messages, { text: "Привет, я Бот!", author: "Bot" }],
+                    name: "Bot",
+                });
+            }, 500);
+        }
     }
 
     reset = () => {
-        this.setState(initialState)
-     }
+        this.setState(initialState);
+    };
+
+    toggle = () => {
+        this.setState(({ visible }) => ({ visible: !visible }));
+    };
+
+    addNewMessage = (message) => {
+        const { messages } = this.state;
+        this.setState({ messages: [...messages, { text: message.text, author: message.author }] });
+    };
 
     render() {
-        const { messages, name } = this.state;
+        const { messages, name, visible } = this.state;
+        const { lastname } = this.props;
+
         return (
             <div>
-                <h2>Hello {name}</h2>
+                {/* this.props.name - если прилетают внешние пропсы */}
+                <h2>{`Hello, ${name} ${lastname}`}</h2>
                 <Example />
-                {messages.map((message, idx) => (
-                    <div key={idx}>{message}</div>
+                {messages.map(({ text, author }, idx) => (
+                    <div key={idx}>{`${author}: ${text}`}</div>
                 ))}
+                <FormMessage addNewMessage={this.addNewMessage} />
                 <button onClick={this.addMessage}>Ckick</button>
                 <button onClick={this.reset}>Reset</button>
+                <button onClick={this.toggle}>Visible</button>
+                {visible && <Counter />}
             </div>
         );
     }
 }
+
+HelloMessage.defaultProps = {
+    lastname: "Ivanov",
+};
+
+HelloMessage.propTypes = {
+    name: PropTypes.string.isRequired,
+    lastname: PropTypes.string.isRequired,
+};
 
 export default HelloMessage;
