1c2069828a431bf59b2c96461629959fcca75096
Elena Ditiateva <elena2091@bk.ru>
2020-05-10 12:26:04 +0300

    DZ-3

--- Dityateva/src/index.jsx
+++ Dityateva/src/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloMessage from './components/HelloMessage';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Layout from './components/Layout';
+import './index.css';
+
+const theme = createMuiTheme({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+});
 
 ReactDOM.render(
-    <HelloMessage />,
-    document.getElementById('hello-react')
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Layout />
+  </ThemeProvider>,
+  document.getElementById('messenger'),
 );
