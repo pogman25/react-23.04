45f87de48eff72dc623655bdc2338acde47b4aa8
DimaLom <lom797@gmail.com>
2020-05-10 22:03:34 +0700

    Разбил конфиги на прод и дев

---
+++ Dmitriy_Lomov/webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: { app: "./src/index.jsx" },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin(),
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"],
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    plugins: ["@babel/plugin-proposal-class-properties"]
+                },
+            },
+        },
+        ],
+    },
+    output: {
+        filename: '[name][hash].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/index.html",
+        }),
+    ],
+};
---
+++ Dmitriy_Lomov/webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: {
+                                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                            },
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000,
+    },
+});
