7f331b635b48356761cf49e24711d779f8971e5b
razor-rz <58488904+razor-rz@users.noreply.github.com>
2020-05-11 15:30:19 +0300

    task3

---
+++ Skolznev/src/components/Messages/Messages.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import cx from "classnames";
+import styles from "./index.css";
+
+class Messages extends Component {
+    render() {
+        const { messages } = this.props;
+        return (
+            <ul className={styles.list}>
+                {messages.map(({ text, author }, index) => (
+                    <li key={index} className={cx(styles.list, {
+                        [styles.colorBot]: author === 'bot',
+                        [styles.colorUser]: author !== 'bot',
+                    })}>
+                        {`${author}: ${text}`}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+Messages.propTypes = {
+    messages: PropTypes.arrayOf(
+        PropTypes.shape({
+            text: PropTypes.string,
+            author: PropTypes.string,
+        })
+    ).isRequired,
+};
+
+export default Messages;
