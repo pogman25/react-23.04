e3a5907f285df406636faea6e15e566ff9e48fb1
mvagulina <marinka_2001@mail.ru>
2020-05-11 15:30:43 +0300

    lesson3 homework

---
+++ vagulina/src/components/Message/Message.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import css from "./index.css";
+import cn from "classnames";
+
+export default class Message extends React.Component {
+  componentDidMount() {
+//    console.log("message did mount");
+  }
+
+  componentDidUpdate() {
+//    console.log("message did update");
+  }
+
+  render() {
+//    console.log("message render");
+    const { author, text } = this.props;
+    return (
+      <div
+        className={cn(css.message, {
+          [css.left]: author === "Robot",
+          [css.right]: author !== "Robot",
+        })}
+      >
+        <div className={css.author}>{author}</div> {text}
+      </div>
+    );
+  }
+}
+
+Message.propTypes = {
+  text: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
--- vagulina/webpack.config.js
+++ vagulina/webpack.config.js
@@ -1,44 +1,64 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports={
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "index[hash].js"
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    module: {
+module.exports = {
+  entry: "./src/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "index[hash].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              plugins: [
-                [
-                    "@babel/plugin-proposal-class-properties",
-                    {
-                        "loose": true
-                    }
-                ]
-              ]
-            }
-          }
-        }
-    ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        compress: true,
-        port: 9000
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: [
+              [
+                "@babel/plugin-proposal-class-properties",
+                {
+                  loose: true,
+                },
+              ],
+            ],
+          },
+        },
       },
-    plugins: [new HtmlWebpackPlugin({  // Also generate a test.html
-        filename: 'index.html',
-        template: 'src/index.html'
-      })]
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
 };
