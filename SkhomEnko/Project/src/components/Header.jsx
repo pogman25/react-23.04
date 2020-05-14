c11541cf07862235cb23487e734d0dbb43295130
Aleksey Skhomenko <a@skhom.ru>
2020-05-14 08:18:40 +0700

    hw4

--- SkhomEnko/Project/src/components/Header.jsx
+++ SkhomEnko/Project/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
-import { AppBar, Toolbar, Typography } from '@material-ui/core'
-import ChatIcon from '@material-ui/icons/Chat'
+import { AppBar, Toolbar, Typography, IconButton as ProfileButton } from '@material-ui/core'
+import { Chat as ChatIcon, AccountBox as ProfileIcon } from '@material-ui/icons/'
+
 
 const mui_theme = createMuiTheme({
   palette: {
     primary: {
       main: '#000'
+    },
+    secondary: {
+      main: '#fff'
     }
   }
 })
@@ -16,10 +21,32 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1
   },
   AppBar: {
-    alignItems: 'center'
+    alignItems: "center"
   },
   icon: {
     verticalAlign: -3
+  },
+  ProfileBtn: {
+    marginRight: 5
+  },
+  Toolbar: {
+    flexGrow: 1,
+    width: "100%",
+    justifyContent: "center"
+  },
+  TitleText: {
+    flexGrow: 1,
+    textAlign: "center"
+  },
+  Link: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    zIndex: theme.zIndex.appBar,
+    textDecoration: "none",
+    color: "white"
   }
 }))
 
@@ -30,10 +57,15 @@ export default function Header() {
     <div className={classes.root}>
       <ThemeProvider theme={mui_theme}>
         <AppBar className={classes.AppBar} position="static">
-          <Toolbar variant="dense">
-            <Typography variant="h5" color="inherit">
+          <Toolbar variant="dense" className={classes.Toolbar}>
+            <Typography variant="h5" color="inherit" className={classes.TitleText}>
+              <Link to="/" key="home-page" className={classes.Link} />
               <ChatIcon className={classes.icon} /> React Educational Messenger
             </Typography>
+            <ProfileButton color="inherit" edge="end" className={classes.ProfileBtn}>
+              <Link to="/profile" key="profile" className={classes.Link} />
+              <ProfileIcon />
+            </ProfileButton>
           </Toolbar>
         </AppBar>
       </ThemeProvider>
