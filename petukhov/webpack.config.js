06f1c1604e53cd8dcaa47465641f32ecc13a8eca
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2020-05-16 21:30:08 +0300

    update

--- petukhov/webpack.config.js
+++ petukhov/webpack.config.js
@@ -1,75 +1,27 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const webpack = require('webpack');
 
 module.exports = {
-  entry: "./src/index.jsx",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].js",
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-            plugins: ["@babel/plugin-proposal-class-properties"],
-          },
-        },
-      },
+   entry: {
+       app: './index.jsx',
+   },
+   context: path.resolve(__dirname, "static_src"),
+   output: {
+       path: path.resolve(__dirname, "static", "build"),
+       filename: 'app.js',
+   },
 
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[path][name]__[local]--[hash:base64:5]",
-              },
-            },
-          },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
-      cacheGroups: {
-        default: false,
-        commons: {
-          test: /node_modules/,
-          name: "js/vendor",
-          chunks: "initial",
-        },
-        styles: {
-          name: "styles",
-          test: /\.css$/,
-          chunks: "all",
-          enforce: true,
-        },
-      },
-    },
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "build"),
-    compress: true,
-    port: 9000,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "src/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+   module: {
+       rules: [
+           {
+               test: /\.(js|jsx)$/,
+               include: path.resolve(__dirname, "static_src"),
+               loader: 'babel-loader',
+               exclude: /node_modules/,
+               options: {
+                 presets: ['@babel/env', '@babel/react'],
+               }
+           },
+       ],
+   },
 };
