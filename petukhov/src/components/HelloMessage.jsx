baf330d1af52803a3a668a97414fbf2965ff0af9
Anton Petukhov <39304158+hydraIV@users.noreply.github.com>
2020-05-16 21:16:20 +0300

    update

--- petukhov/src/components/HelloMessage.jsx
+++ petukhov/src/components/HelloMessage.jsx
@@ -1,37 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-class HelloMessage extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      messages: [],
-    };
-
-    this.addMessage = this.addMessage.bind(this);
+class HelloMessage extends Component {
+  state = {
+    messages: [],
   }
 
-  addMessage() {
-    this.setState(prev => ({ messages: [...prev.messages, "Нормально"] }));
-  }
+  addMessage = () => {
+    this.setState(({messages}) => ({ messages: [...messages, "Привет!"] }));
+  };
 
   render() {
-    const { messages } = this.state;
-    const messagesList = messages.map((item, index) => {
-        return (
-            <li key = {index}>{item}</li>
-            )
-    });
+    const messages = this.state.messages;
     return (
       <div>
-        <h2>Hello, {this.props.name}!</h2>
-        <button onClick={this.addMessage}>Add list item</button>
-        <ul>
-            {messagesList}
-        </ul>
+        <h2>Привет</h2>
+        <button onClick={this.addMessage}>Add</button>
+          <ul>
+            {messages.map((message, index) => (
+              <li key={index}>
+                <p>{message}</p>
+              </li>
+            ))}
+          </ul>
       </div>
     );
   }
-}
+};
 
 export default HelloMessage;
