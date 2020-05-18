e26a88ab2357ef0c138dfd5d7242513fa4aa30e4
Погорелов Александр <a.pogorelov@zebrains.team>
2020-05-18 21:56:40 +0300

    lesson 6 start

--- Pogorelov/src/store/index.js
+++ Pogorelov/src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from '../reducers';
+import botAnswer from './botAnswer';
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+
+const store = createStore(rootReducer, applyMiddleware(logger, botAnswer));
 
 export default store;
