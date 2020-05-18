1844f1f072008da7763c31f8afe32a6ea8315016
mvagulina <marinka_2001@mail.ru>
2020-05-18 12:27:08 +0300

    Добавление нового чата

--- vagulina/src/components/Layout/Layout.jsx
+++ vagulina/src/components/Layout/Layout.jsx
@@ -6,16 +6,74 @@ import Header from "../Header";
 import css from "./index.css";
 
 class Layout extends React.Component {
+  ROBOT_NAME = "Robot";
+
+  state = {
+    chats: {
+      1: { title: "Chat1", messageList: [1, 2] },
+      2: { title: "Chat2", messageList: [] },
+      3: { title: "Chat3", messageList: [] },
+      4: { title: "Chat4", messageList: [] },
+    },
+    messages: {
+      1: { text: "Hello!", author: this.ROBOT_NAME },
+      2: { text: "How do you do?", author: this.ROBOT_NAME },
+    },
+  };
+
+  componentDidUpdate() {
+    const { chatId } = this.props;
+    const { messages, chats } = this.state;
+    const messageList = chats[chatId].messageList;
+    if (messageList.length > 0) {
+      const lastMsgId = messageList[messageList.length - 1];
+      if (messages[lastMsgId].author !== this.ROBOT_NAME) {
+        this.addNewMessage({ text: "This is bot...", author: this.ROBOT_NAME });
+      }
+    }
+  }
+
+  addNewMessage = (message) => {
+    const { chatId } = this.props;
+    const newMessageId = Object.keys(this.state.messages).length + 1;
+    this.setState(({ messages, chats }) => ({
+      messages: {
+        ...messages,
+        [newMessageId]: { text: message.text, author: message.author },
+      },
+      chats: {
+        ...chats,
+        [chatId]: {
+          ...chats[chatId],
+          messageList: [...chats[chatId]["messageList"], newMessageId],
+        },
+      },
+    }));
+  };
+
+  addChat = (title) => {
+    const { chats } = this.state;
+    const chatId = Object.keys(chats).length + 1;
+    this.setState({
+      chats: { ...chats, [chatId]: { title: title, messageList: [] } },
+    });
+  };
+
   render() {
     return (
       <div className={css.layout}>
         <Header chatId={this.props.chatId} />
         <div className={css.canvas}>
           <div className="layout-left-side">
-            <ChatList />
+            <ChatList chats={this.state.chats} addChat={this.addChat} />
           </div>
           <div className="layout-right-side">
-            <MessageField chatId={this.props.chatId} />
+            <MessageField
+              chatId={this.props.chatId}
+              chats={this.state.chats}
+              messages={this.state.messages}
+              addNewMessage={this.addNewMessage}
+            />
           </div>
         </div>
       </div>
--- vagulina/src/components/MessageField/MessageField.jsx
+++ vagulina/src/components/MessageField/MessageField.jsx
@@ -4,59 +4,14 @@ import MessageForm from "../MessageForm";
 import css from "./index.css";
 import PropTypes from "prop-types";
 
-export default class MessageField extends Component {
-  ROBOT_NAME = "Robot";
-
-  state = {
-    chats: {
-      1: { title: "Chat1", messageList: [1, 2] },
-      2: { title: "Chat2", messageList: [] },
-      3: { title: "Chat3", messageList: [] },
-    },
-    messages: {
-      1: { text: "Hello!", author: this.ROBOT_NAME },
-      2: { text: "How do you do?", author: this.ROBOT_NAME },
-    },
-  };
-
+class MessageField extends Component {
   componentDidMount() {
     //    console.log("MessageField did mount");
   }
 
-  componentDidUpdate() {
-    const { chatId } = this.props;
-    const { messages, chats } = this.state;
-    const messageList = chats[chatId].messageList;
-    if (messageList.length > 0) {
-      const lastMsgId = messageList[messageList.length - 1];
-      if (messages[lastMsgId].author !== this.ROBOT_NAME) {
-        this.addNewMessage({ text: "This is bot...", author: this.ROBOT_NAME });
-      }
-    }
-  }
-
-  addNewMessage = (message) => {
-    const { chatId } = this.props;
-    const newMessageId = Object.keys(this.state.messages).length + 1;
-    this.setState(({ messages, chats }) => ({
-      messages: {
-        ...messages,
-        [newMessageId]: { text: message.text, author: message.author },
-      },
-      chats: {
-        ...chats,
-        [chatId]: {
-          ...chats[chatId],
-          messageList: [...chats[chatId]["messageList"], newMessageId],
-        },
-      },
-    }));
-  };
-
   render() {
     //    console.log(`MessageField render`);
-    const { messages, chats } = this.state;
-    const { chatId } = this.props;
+    const { chatId, messages, chats, addNewMessage } = this.props;
 
     const messageElements = chats[chatId].messageList.map((messageId, id) => (
       <Message
@@ -69,7 +24,7 @@ export default class MessageField extends Component {
       <div className={css.container}>
         <div className={css.elements}>
           {messageElements}
-          <MessageForm addNewMessage={this.addNewMessage} />
+          <MessageForm addNewMessage={addNewMessage} />
         </div>
       </div>
     );
@@ -78,4 +33,9 @@ export default class MessageField extends Component {
 
 MessageField.propTypes = {
   chatId: PropTypes.number.isRequired,
+  chats: PropTypes.object.isRequired,
+  messages: PropTypes.object.isRequired,
+  addNewMessage: PropTypes.func.isRequired,
 };
+
+export default MessageField;
