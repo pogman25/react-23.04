6da852ae37b5d7a66cee2e5d2f02181853e43d3a
Konfeta <Vale@iMac.local>
2020-05-21 15:34:20 +0300

    Домашнее задание к Уроку 4 (неполное)

--- Valentina Kunina/src/pages/Chats/ChatsFunctional.jsx
+++ Valentina Kunina/src/pages/Chats/ChatsFunctional.jsx
@@ -1,8 +1,8 @@
 import React, { useState, memo, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import Example from "../Archive/Example/Example";
+import Example from "../../components/Archive/Example/Example";
 // import Counter from "../Counter";
-import FormMessage from "../FormMessage";
+import FormMessage from "../../components/FormMessage";
 import { makeStyles } from '@material-ui/core/styles';
 import style from '../../index.css';
 
