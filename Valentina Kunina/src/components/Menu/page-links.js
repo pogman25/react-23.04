25bcfc11903b9103dca517ebadf0920943eb7023
Konfeta <Vale@iMac.local>
2020-05-25 15:30:25 +0300

    WIP Домашнее задание к Уроку 4 (незаконченное)

--- Valentina Kunina/src/components/Menu/page-links.js
+++ Valentina Kunina/src/components/Menu/page-links.js
@@ -1,13 +1,11 @@
 const pageLinks = [
     {
-        to: '/',
-        title: 'Home',
-    },
-    {
+        id: 1,
         to: '/chat/1',
         title: 'Chat 1',
     },
     {
+        id: 2,
         to: '/chat/2',
         title: 'Chat 2',
     }
--- Valentina Kunina/webpack.config.js
+++ Valentina Kunina/webpack.config.js
@@ -2,77 +2,78 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-module.exports = (env, { mode = 'development'}) => {
+module.exports = (env, { mode = "development" }) => {
     const isDev = mode === "development";
     return {
-    entry: "./src/index.jsx",
-    output: {
-        path: path.resolve(__dirname, "build"), //название папки output
-        filename: "[name].js", //можно назвать файл app.js
-    },
-    resolve: {
-        extensions: [".js", ".jsx"], //чтобы вебпак мог подхватить jsx-файлы
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/, //какие файлы с каким расширением брать
-                exclude: /(node_modules)/,
-                use: {
-                    loader: "babel-loader",
-                },
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: isDev
-                                ? {
-                                    localIdentName: "[path][name]__[local]--[hash:base64:5]",
-                                }
-                                : {
-                                    localIdentName: "[hash:base64:5]",
-                                  },
-                        },
+        entry: "./src/index.jsx",
+        output: {
+            path: path.resolve(__dirname, "build"), //название папки output
+            filename: "[name].js", //можно назвать файл app.js
+            publicPath: "/" ,
+        },
+        resolve: {
+            extensions: [".js", ".jsx"], //чтобы вебпак мог подхватить jsx-файлы
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/, //какие файлы с каким расширением брать
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: "babel-loader",
                     },
-                ],
-            },
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-            cacheGroups: {
-                default: false,
-                commons: {
-                    test: /node_modules/,
-                    name: "js/vendor",
-                    chunks: "initial",
                 },
-                styles: {
-                    name: "styles",
-                    test: /\.css$/,
-                    chunks: "all",
-                    enforce: true,
+                {
+                    test: /\.css$/i,
+                    use: [
+                        isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: isDev
+                                    ? {
+                                          localIdentName: "[path][name]__[local]--[hash:base64:5]",
+                                      }
+                                    : {
+                                          localIdentName: "[hash:base64:5]",
+                                      },
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        optimization: {
+            splitChunks: {
+                chunks: "all",
+                cacheGroups: {
+                    default: false,
+                    commons: {
+                        test: /node_modules/,
+                        name: "js/vendor",
+                        chunks: "initial",
+                    },
+                    styles: {
+                        name: "styles",
+                        test: /\.css$/,
+                        chunks: "all",
+                        enforce: true,
+                    },
                 },
             },
         },
-    },
-    devServer: {
-        contentBase: path.join(__dirname, "build"),
-        compress: true,
-        port: 8080,
-        historyApiFallback: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "src/index.html",
-        }),
-        new MiniCssExtractPlugin(),
-    ],
+        devServer: {
+            contentBase: path.join(__dirname, "build"),
+            compress: true,
+            port: 8080,
+            historyApiFallback: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: "index.html",
+                template: "src/index.html",
+            }),
+            new MiniCssExtractPlugin(),
+        ],
+    };
 };
-}
