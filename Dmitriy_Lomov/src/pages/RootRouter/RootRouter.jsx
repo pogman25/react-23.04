845ab20e1e6b1cd5aafc90e6aa40ab13be57b382
DimaLom <lom797@gmail.com>
2020-05-28 00:02:48 +0700

    add workbox and storybook

--- Dmitriy_Lomov/src/pages/RootRouter/RootRouter.jsx
+++ Dmitriy_Lomov/src/pages/RootRouter/RootRouter.jsx
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 
 import { fetchChatsData } from '../../store/chats/actions';
+import { fetchMessagesData } from '../../store/messages/actions';
+
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Home from '../Home';
 import Chats from '../Chats';
@@ -11,14 +15,20 @@ import EmptyPage from '../EmptyPage';
 
 class RootRouter extends Component {
   componentDidMount() {
-    const { fetchChatsData } = this.props;
-
+    const { fetchChatsData, fetchMessagesData } = this.props;
+    
     fetchChatsData();
+    fetchMessagesData();
   }
 
   render() {
+    const { open } = this.props;
+
     return (
       <Layout>
+        <Backdrop open={open}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/chats/:chatId" component={Chats} />
@@ -31,8 +41,13 @@ class RootRouter extends Component {
   }
 }
 
+const mapStateToProps = store => ({
+  open: store.chats.isFetching,
+});
+
 const mapDispatchToProps = {
   fetchChatsData,
+  fetchMessagesData,
 };
 
-export default connect(null, mapDispatchToProps)(RootRouter);
+export default connect(mapStateToProps, mapDispatchToProps)(RootRouter);
