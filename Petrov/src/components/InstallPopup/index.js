3eac32a6c8ad6b13693e38448013ac818779ba3b
WetShark <7393422@mail.ru>
2020-05-28 11:27:33 +0300

    dz-8

---
+++ Petrov/src/components/InstallPopup/index.js
@@ -0,0 +1,1 @@
+export { default } from './InstallPopup';
--- Petrov/src/index.jsx
+++ Petrov/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import RootRouter from './pages/RootRouter';
+import InstallPopup from './components/InstallPopup';
+import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 
@@ -10,7 +12,9 @@ ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
   <RootRouter />
+  <InstallPopup />
   </BrowserRouter>
   </Provider>,
   document.getElementById("hello-example")
 );
+serviceWorker.register();
